d750cba36baf74d4a6a6af853af998ab46fa370a
Kuba S <jsmolka@oakfusion.pl>
2022-02-16 12:57:00 +0100

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,8 @@
 module.exports = {
-    siteMetadata: {
-      title: `My Gatsby Site`,
-        siteUrl: `https://www.yourdomain.tld`,
-    },
-    plugins: [
-
-    ]
-}
+  siteMetadata: {
+    title: `My Gatsby Site`,
+    siteUrl: `https://www.yourdomain.tld`,
+    someText: `hello!`,
+  },
+  plugins: ["gatsby-plugin-styled-components"],
+};
